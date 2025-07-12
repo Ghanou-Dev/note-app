7809e6e0af79a16588bb6bd98580a354645125c4
Ghanou-Dev <sgan36g@gmail.com>
2025-07-12 12:49:29 +0100

    add background color to reminders page

--- lib/pages/reminders.dart
+++ lib/pages/reminders.dart
@@ -7,7 +7,9 @@ class Reminders extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: primaryColor,
       appBar: AppBar(
+        backgroundColor: primaryColor,
         title: Text("Reminders", style: TextStyle(fontWeight: FontWeight.bold)),
       ),
       body: Center(
